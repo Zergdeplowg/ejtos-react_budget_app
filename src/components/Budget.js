8a5edf2e4081d8e28144c8fb0abd4cdf28465c94
Zergdeplowg <josterhus@comcast.net>
2023-05-29 13:36:09 -0400

    Add input to budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const setBudget = (value) =>{
+        
+    };
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input
+                        required='required'
+                        type='number'
+                        step='10'
+                        max="20000"
+                        id='cost'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input></span>
         </div>
     );
 };
